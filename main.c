f663894a1a0c5e17bc5239413d4af3f5def27a63
Denis Perdomo <joanp307@gmail.com>
2026-05-23 11:52:26 -0400

    first commit

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+typedef struct {
+
+} Tree;
+
+Tree tokenize_source(const char* source) {
+    const Tree result = {};
+    
+    return result;
+}
+
+int main(void) {
+    char* sample_source = "function 1 nested-function 123 variable";
+
+    printf("Hello, World!\n");
+
+    return 0;
+}
