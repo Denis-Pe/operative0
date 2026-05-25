55a282de4ab691c50438cef3fa3fecc46fa35ca4
Denis Perdomo <joanp307@gmail.com>
2026-05-24 21:49:03 -0400

    basic word parsing

--- main.c
+++ main.c
@@ -3,20 +3,70 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define DEFAULT_STRING_CAPACITY 8
+
 /// Owned string, not null-terminated
 typedef struct {
     char *ptr;
     size_t len;
     size_t cap;
-} OString;
+} String;
 
-    
+/// initialized to \0
+/// buffer is set to NULL if capacity is 0
+String str_make_capacity(const size_t capacity) {
+    char *buf = NULL;
+    if (capacity > 0) {
+        buf = malloc(capacity);
+    }
+    const String s = {buf, 0, capacity};
+    return s;
+}
+
+String str_make() {
+    return str_make_capacity(DEFAULT_STRING_CAPACITY);
+}
+
+void str_pushc(String *s, const char c) {
+    if (s->len == s->cap) {
+        s->ptr = realloc(s->ptr, s->cap * 2);
+    }
+    s->ptr[s->len] = c;
+    s->len++;
+}
+
+void str_pushtstr(String *string, const char *tstr) {
+    for (const char *tstrptr = tstr; *tstrptr != '\0'; tstrptr++) {
+        str_pushc(string, *tstrptr);
+    }
+}
+
+String str_fromtstr(const char *tstr) {
+    String s = str_make(8);
+    str_pushtstr(&s, tstr);
+    return s;
+}
+
+String str_clone(const String from) {
+    char *buf = malloc(from.cap);
+    memcpy(buf, from.ptr, from.len);
+    const String s = {buf, from.len, from.cap};
+    return s;
+}
 
 /// Borrowed string, not null-terminated
 typedef struct {
     const char *ptr;
     size_t len;
-} BString;
+} StringView;
+
+StringView strv_fromtstr(const char *tstr) {
+    StringView strv = {tstr, 0};
+    for (const char *tstrptr = tstr; *tstrptr != '\0'; tstrptr++) {
+        strv.len++;
+    }
+    return strv;
+}
 
 typedef enum {
     FORM_WORD,
@@ -25,7 +75,7 @@ typedef enum {
 
 typedef struct {
     union {
-        OString as_word;
+        String as_word;
         long as_long;
     };
 
@@ -33,22 +83,68 @@ typedef struct {
 } Form;
 
 typedef enum {
-    INIT,
+    PARSING_NONE,
     PARSING_WORD,
     PARSING_LONG,
 } ParsingState;
 
-/**
- * Parses a form until the next whitespace or the end of the string
- * @return
- */
-Form parse_form(const BString src) {
+Form parse_form(const StringView src) {
+    ParsingState state = PARSING_NONE;
+    Form form;
+
+    for (size_t i = 0; i < src.len; ++i) {
+        const char c = src.ptr[i];
+        switch (state) {
+            case PARSING_NONE:
+                if (isalpha(c)) {
+                    state = PARSING_WORD;
+                    form.type = FORM_WORD;
+                    form.as_word = str_make();
+                    str_pushc(&form.as_word, c);
+                } else if (isdigit(c)) {
+                    state = PARSING_LONG;
+                    form.type = FORM_LONG;
+                    form.as_long = c - 48;
+                } else if (isspace(c)) {
+                    continue;
+                } else {
+                    printf("Unsupported character: '%c'\nExiting now...\n", c);
+                    exit(-1);
+                }
+                break;
+            case PARSING_WORD:
+                if (isalnum(c)) {
+                    str_pushc(&form.as_word, c);
+                } else if (isspace(c)) {
+                    state = PARSING_NONE;
+                } else {
+                    printf("Unsupported character: '%c'\nExiting now...\n", c);
+                    exit(-1);
+                }
+                break;
+            case PARSING_LONG:
+                if (isdigit(c)) {
+                    form.as_long *= 10;
+                    form.as_long += c - 48;
+                } else if (isspace(c)) {
+                    state = PARSING_NONE;
+                } else {
+                    printf("Unsupported character: '%c'\nExiting now...\n", c);
+                    exit(-1);
+                }
+                break;
+        }
+    }
+    return form;
 }
 
 int main(void) {
-    char *sample_source = "function 1 nested-function 123 variable";
+    StringView sample_source = strv_fromtstr("function-");
 
     printf("Hello, World!\n");
+
+    Form f = parse_form(sample_source);
+    printf("%i %.*s\n", f.type, f.as_word.len, f.as_word.ptr);
 
     return 0;
 }
