4f685d58738c92889d53932b8225ac7fdae7e02d
Denis Perdomo <joanp307@gmail.com>
2026-07-12 13:31:05 -0400

    new Seq type for dynamic array boilerplate

---
+++ include/seq.h
@@ -0,0 +1,41 @@
+//
+// Created by denis on 7/12/26.
+//
+
+#ifndef OPERATIVE_SEQ_H
+#define OPERATIVE_SEQ_H
+
+#define DEFAULT_SEQ_SIZE 8
+
+#include <stdlib.h>
+
+#define DEFINE_SEQ(SeqType, fun_spec, ElemType) typedef struct {\
+    ElemType* ptr;\
+    size_t len;\
+    size_t cap;\
+} SeqType;\
+SeqType alloc_##fun_spec##_cap(const size_t cap) {\
+    ElemType *ptr = malloc(sizeof(ElemType) * cap);\
+    const SeqType seq = {ptr, 0, cap};\
+    return seq;\
+}\
+SeqType alloc_##fun_spec() {\
+    return alloc_##fun_spec##_cap(DEFAULT_SEQ_SIZE);\
+}\
+void fun_spec##_push(SeqType *seq, const ElemType *elem) {\
+    if (seq->len == seq->cap) {\
+        size_t new_cap = seq->cap? seq->cap*2 : DEFAULT_SEQ_SIZE;\
+        seq->ptr = realloc(seq->ptr, new_cap * sizeof(ElemType));\
+        seq->cap = new_cap;\
+    }\
+    seq->ptr[seq->len++] = *elem;\
+}//\
+// void free_tokens(const SeqType seq) {\
+//      for (size_t i = 0; i < seq.len; i++) {\
+//          free_token(seq.ptr[i]);\
+//      }\
+//      free(seq.ptr);\
+// }
+
+
+#endif //OPERATIVE_SEQ_H
