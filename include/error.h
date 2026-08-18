49ed8827c86226d957d79c1ba6d2205e7dbc2a11
Denis Perdomo <joanp307@gmail.com>
2026-08-18 15:23:52 -0400

    new error for inexhaustive switches

--- include/error.h
+++ include/error.h
@@ -5,4 +5,6 @@ void panicf(const char* format, ...);
 void panic_errno(void);
 void panic(void);
 
+#define panic_switch() panicf("Error: switch statement is inexhaustive in %s %s\n", __FILE__, __LINE__)
+
 #endif //OPERATIVE_ERROR_H
