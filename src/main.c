9e088ef00615652c2fe83a093207823d9ba5314a
Denis Perdomo <joanp307@gmail.com>
2026-08-30 19:36:47 -0400

    basic code for starting to actually evaluate code!

--- src/main.c
+++ src/main.c
@@ -1,5 +1,139 @@
+#include <assert.h>
+
 #include "string/string.h"
 #include "parsing.h"
+#include "seq.h"
+
+typedef struct Value Value;
+
+typedef StringView Word;
+
+typedef struct {
+    Value *elements;
+    size_t len;
+} Block;
+
+enum ValueType {
+    TYPE_BLOCK,
+    TYPE_WORD,
+    TYPE_INT,
+    TYPE_DOUBLE
+};
+
+struct Value {
+    enum ValueType type;
+
+    union {
+        Word as_word;
+        int64_t as_integer;
+        double as_double;
+        Block as_block;
+    };
+};
+
+void printval(const Value v) {
+    switch (v.type) {
+        case TYPE_WORD:
+            fprintstrv(stdout, v.as_word);
+            printf("\n");
+            break;
+        case TYPE_DOUBLE:
+            printf("%lf\n", v.as_double);
+            break;
+        case TYPE_INT:
+            printf("%zu", v.as_integer);
+            break;
+        default:
+            panic_switch();
+    }
+}
+
+typedef struct {
+    Word word;
+    Value value;
+} Binding;
+
+DECLARE_SEQ(ScopeBindings, bindings, Binding)
+
+DEFINE_SEQ(ScopeBindings, bindings, Binding)
+
+typedef struct Environment Environment;
+
+struct Environment {
+    Environment *parent;
+    ScopeBindings bindings;
+};
+
+Environment alloc_root_env(void) {
+    return (Environment){NULL, alloc_bindings()};
+}
+
+Environment alloc_subenv(Environment *parent) {
+    return (Environment){parent, alloc_bindings()};
+}
+
+void free_env(const Environment env) {
+    free(env.bindings.ptr);
+}
+
+typedef Value (*OperativeFun)(Environment curr, Block args);
+
+typedef struct {
+    OperativeFun fun;
+    size_t expected_args;
+} Operative;
+
+Value identity(const Environment curr, const Block args) {
+    return (Value){.type = TYPE_BLOCK, .as_block = args};
+}
+
+Value set_value(Environment curr, const Block args) {
+    assert(args.elements[0].type == TYPE_WORD);
+
+    const Binding b = (Binding){
+        .word = args.elements[0].as_word,
+        .value = args.elements[1]
+    };
+    bindings_push(&curr.bindings, &b);
+
+    return (Value){.type = TYPE_INT, .as_integer = 0};
+}
+
+Value eval_ast(ASTBlock root);
+
+Value eval_form(const ASTNode form) {
+    Value result;
+
+    switch (form.type) {
+        case AST_WORD:
+            result.type = TYPE_WORD;
+            result.as_word = form.as_word;
+            break;
+        case AST_DOUBLE:
+            result.type = TYPE_DOUBLE;
+            result.as_double = form.as_double;
+            break;
+        case AST_INTEGER:
+            result.type = TYPE_INT;
+            result.as_integer = form.as_integer;
+            break;
+        case AST_BLOCK:
+            result = eval_ast(form.as_block);
+            break;
+    }
+
+    return result;
+}
+
+Value eval_ast(const ASTBlock root) {
+    Value result;
+
+    for (size_t i = 0; i < root.len; i++) {
+        result = eval_form(root.ptr[i]);
+    }
+
+    return result;
+}
 
 int main(void) {
     const StringView sample_source = strv_fromcstr(
@@ -10,8 +144,9 @@ int main(void) {
     size_t parsing_index = 0;
     const ASTBlock root = parse_tokens((TokensSlice){tokens.ptr, tokens.len}, &parsing_index);
 
-    printast(root, 0);
-
+    // printast(root, 0);
+    const Value v = eval_ast(root);
+    printval(v);
 
     free_tokens(tokens);
     free_block(root);
